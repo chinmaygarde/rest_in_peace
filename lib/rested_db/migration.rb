c6b3b8ebaf4c0ed87f884f5505ef9cd2eb7db997
Chinmay Garde <chinmaygarde@gmail.com>
2010-02-24 02:31:26 +0530

    Progress on the server startup routine. Lots of cleanup required.

--- lib/rested_db/migration.rb
+++ lib/rested_db/migration.rb
@@ -5,16 +5,18 @@ class Migration
   
   def migrate(project_root)
     settings = ProjectSettings.new(project_root)
+    count = 0
     Dir.foreach(settings.model_directory) do |m|
       file_path = File.join(settings.model_directory, m)
       full_path = File.expand_path(file_path)
       unless File.directory?(full_path)
         require "#{full_path}"
+        count = count + 1
       end
     end
     DataMapper.setup(:default, "sqlite3://#{settings.database_directory}/development.sqlite3")
     DataMapper.auto_migrate!
-    puts "Migrations Complete"
+    puts "All #{count} Migrations Complete"
   end
   
 end
--- lib/rested_db/server_manager.rb
+++ lib/rested_db/server_manager.rb
@@ -1,7 +1,20 @@
 class ServerManager
   
-  def ServerManager.run(root_dir)
-    p "Server is running! Nah, just kidding"
+  def start_server(project_root)
+    
+    settings = ProjectSettings.new(project_root)
+
+    # Load all controllers
+    Dir.foreach(settings.controller_directory) do |c|
+      file_path = File.join(settings.controller_directory, c)
+      full_path = File.expand_path(file_path)
+      unless File.directory?(full_path)
+        load "#{full_path}"
+      end
+    end
+    
+    Sinatra::Server.new.start
+    
   end
   
 end
